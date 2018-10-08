c1127a891f8e884f43603596586ee4675854a282
Dmitry Grigoryev <dmitry.grigoryev@gmx.de>
2018-10-08 21:18:21 +0200

    commit after lection

--- src/AppTransformer.java
+++ src/AppTransformer.java
@@ -4,24 +4,24 @@ public class AppTransformer {
 
     public static void main(String[] args) {
 
-        String sentence = "Ajsh jFTd yf bbb LLL kRR pSl DGFF inskyline  vbu okjvgfxfi";
+        String sentence = "Ajsh jFTd yf bbb LLL kRR pSl DGFF InSkyLine  vbu okjvHESfi";
 
         Transform t = new Transform();
 
         Predicate<String> isWordLenght_3 = str -> str.length() == 3;
         Predicate<String> isWordLenght_4 = str -> str.length() == 4;
-        Predicate<String> isWordLenght_9 = str -> str.length() == 9;
+       // Predicate<String> isWordLenght_9 = str -> str.length() == 9;
 
         Function<String, String> transformationMethodToUpperCase = str -> str = str.toUpperCase();
         Function<String, String> transformationMethodToLowerCase = str -> str = str.toLowerCase();
-        Function<String, String> transformationMethodToStars = str -> str = "*******";
+     //   Function<String, String> transformationMethodToStars = str -> str = "*******";
 
 
         t.print(sentence, isWordLenght_3, transformationMethodToUpperCase);
         t.print(sentence, isWordLenght_4, transformationMethodToLowerCase);
-        t.print(sentence, isWordLenght_9, transformationMethodToStars);
-        t.print(sentence, isWordLenght_9, transformationMethodToUpperCase);
-        t.print(sentence, isWordLenght_9, transformationMethodToLowerCase);
+       // t.print(sentence, isWordLenght_9, transformationMethodToStars);
+      //  t.print(sentence, isWordLenght_9, transformationMethodToUpperCase);
+       // t.print(sentence, isWordLenght_9, transformationMethodToLowerCase);
 
     }
 }
--- src/Transform.java
+++ src/Transform.java
@@ -1,22 +1,38 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.StringJoiner;
 import java.util.function.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Transform {
 
 
-    private String transform(String sentence, Predicate<String> checkWordLenght, Function<String, String> transformationMethod) {
+    private String transform(String sentence, Predicate<String> checkWordLenght, Function<String, String> transfornationMethod) {
         String words[] = sentence.split(" ");
-        List<String> list = Arrays.asList(words);
-        return list
+
+        return Stream.of(words)
+                .map(s->checkWordLenght.test(s)?transfornationMethod.apply(s):s)
+        .collect(Collectors.joining(", "));
+
+/*      List<String> list = Arrays.asList(words);
+        finalString =  list
                 .stream()
-                .filter(checkWordLenght)
-                .map(s -> transformationMethod.apply(s))
+                .filter(s->checkWordLenght.test(s))
+                .map(s->s=transfornationMethod.apply(s))
                 .collect(Collectors.joining(", "));
+
+
+        for (String word : words) {
+            if (checkWordLenght.test(word)) {
+                word = transfornationMethod.apply(word);
+            }
+            finalString += word + ", ";
+        }*/
+
     }
 
-    public void print(String s, Predicate<String> check, Function<String, String> transform) {
-        System.out.println(transform(s, check, transform));
+    public void print(String s, Predicate<String> ch, Function<String, String> tr) {
+        System.out.println(transform(s, ch, tr));
     }
 }
